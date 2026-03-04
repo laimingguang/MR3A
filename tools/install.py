aa0fbe68463b4bf93bd93f97fd710eb2cd2f1e13
originalsage <lls2233@qq.com>
2026-03-04 17:39:41 +0800

    feat:尝试构建agent

--- tools/install.py
+++ tools/install.py
@@ -133,6 +133,22 @@ def install_agent():
         dirs_exist_ok=True,
     )
 
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        import json
+        interface = json.load(f)
+
+    if os_name == "win":
+        interface["agent"]["child_exec"] = r"./python/python.exe"
+    elif os_name == "macos":
+        interface["agent"]["child_exec"] = r"./python/bin/python3"
+    elif os_name == "linux":
+        interface["agent"]["child_exec"] = r"python3"
+
+    interface["agent"]["child_args"] = ["-u", r"./agent/main.py"]
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        json.dump(interface, f, ensure_ascii=False, indent=4)
+
 
 if __name__ == "__main__":
     install_deps()
